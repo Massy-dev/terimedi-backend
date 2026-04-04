8768b0e427dad6a99195c8ab790a1ba5656a0a94
Massy-dev <massy1833.dev@gmail.com>
2026-04-04 19:23:27 +0000

    Resolution de bug GDAL_LIBRARY 11

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,36 @@ set -e
 
 echo "🔥 PROD SETTINGS LOADED 🔥"
 
-# Vérifier si migrations sont demandées (optionnel)
 if [ "$RUN_MIGRATIONS" = "true" ]; then
     echo "📦 Running migrations..."
     python manage.py migrate --noinput
 fi
 
+# ✅ Création du superuser automatique
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+    echo "👤 Creating superuser..."
+
+    python manage.py shell << END
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+phone = os.getenv("DJANGO_SUPERUSER_PHONE", "0779346330")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "1833production")
+
+if not User.objects.filter(phone=phone).exists():
+    User.objects.create_superuser(phone=phone, password=password)
+    print("✅ Superuser created")
+else:
+    print("ℹ️ Superuser already exists")
+END
+fi
 
 echo "📂 Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "🚀 Starting Daphne..."
 echo "PORT is $PORT"
-echo "Starting Daphne..."
+
 exec daphne -b 0.0.0.0 -p ${PORT} config.asgi:application --verbosity 2
